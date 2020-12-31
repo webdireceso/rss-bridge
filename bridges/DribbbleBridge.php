759aebdae6ab4715201f037b95c272da7bd36028
webdireceso <57294350+webdireceso@users.noreply.github.com>
2020-12-31 22:41:41 +0300

    Update DribbbleBridge.php

--- bridges/DribbbleBridge.php
+++ bridges/DribbbleBridge.php
@@ -86,7 +86,7 @@ favicon-63b2904a073c89b52b19aa08cebc16a154bcf83fee8ecc6439968b1e6db569c7.ico';
 
 	private function getImageTag($preview_path, $title){
 		return sprintf(
-			'<br /> <a href="%s"><img src="%s" alt="%s" /></a>',
+			'<br /> <a href="%s"><img srcset="%s" alt="%s" /></a>',
 			$this->getFullSizeImagePath($preview_path),
 			$preview_path,
 			$title
@@ -94,6 +94,15 @@ favicon-63b2904a073c89b52b19aa08cebc16a154bcf83fee8ecc6439968b1e6db569c7.ico';
 	}
 
 	private function getFullSizeImagePath($preview_path){
-		return explode('?compress=1', $preview_path)[0];
+		// Get last image from srcset
+		$src_set_urls = explode(',', $preview_path);
+		$url = $this->getArrayLastElement($src_set_urls);
+		$url = explode(' ', $url)[1];
+
+		return htmlspecialchars_decode($url);
+	}
+
+	private function getArrayLastElement($array) {
+		return array_values(array_slice($array, -1))[0];
 	}
 }
